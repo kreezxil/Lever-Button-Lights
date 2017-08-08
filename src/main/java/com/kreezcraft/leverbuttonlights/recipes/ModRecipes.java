e721f844f0316bb48b73347131db55dd5a3e4f81
Kreezxil <kreezxil@gmail.com>
2017-08-08 18:06:11 -0500

    1.11.2 commit
    git-ignored recipes folder, used GameRegistry.addShapedRecipe to add the
    two recipes.

--- src/main/java/com/kreezcraft/leverbuttonlights/recipes/ModRecipes.java
+++ src/main/java/com/kreezcraft/leverbuttonlights/recipes/ModRecipes.java
@@ -1,5 +1,7 @@
 package com.kreezcraft.leverbuttonlights.recipes;
 
+import com.kreezcraft.leverbuttonlights.blocks.InitBlocks;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -14,6 +16,9 @@ public class ModRecipes {
 
 	public static void init() {
 		//GameRegistry.addSmelting(InitBlocks.DIAMOND_SAND, new ItemStack(InitBlocks.DIAMOND_GLASS), 10f);
+		GameRegistry.addShapedRecipe(new ItemStack(InitBlocks.GLASS_LEVER), "s","g", 's', Items.STICK, 'g', Blocks.GLASS);
+		GameRegistry.addShapedRecipe(new ItemStack(InitBlocks.GLASS_LIGHT_BLOCK), "t","g",'t',Blocks.TORCH,'g',Blocks.GLASS);
+		
 	}
 
 }
