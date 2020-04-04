a4d714e716a4e8e72d10eacb89d230a65b70598b
Brach Speicher <doomrater@gmail.com>
2020-04-04 13:40:18 -0400

    Update bounding box logic for buttons
    A chimera of the lever and button logic, but works in my build.  Should fix #2.

--- src/main/java/com/kreezcraft/leverbuttonlights/blocks/ColoredLightButton.java
+++ src/main/java/com/kreezcraft/leverbuttonlights/blocks/ColoredLightButton.java
@@ -1,6 +1,11 @@
 package com.kreezcraft.leverbuttonlights.blocks;
 
+import net.minecraft.block.BlockLever;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class ColoredLightButton extends GlassLever {
     protected static final AxisAlignedBB AABB_DOWN_OFF = new AxisAlignedBB(0.3125D, 0.875D, 0.375D, 0.6875D, 1.0D, 0.625D);
@@ -15,9 +20,34 @@ public class ColoredLightButton extends GlassLever {
     protected static final AxisAlignedBB AABB_SOUTH_ON = new AxisAlignedBB(0.3125D, 0.375D, 0.0D, 0.6875D, 0.625D, 0.0625D);
     protected static final AxisAlignedBB AABB_WEST_ON = new AxisAlignedBB(0.9375D, 0.375D, 0.3125D, 1.0D, 0.625D, 0.6875D);
     protected static final AxisAlignedBB AABB_EAST_ON = new AxisAlignedBB(0.0D, 0.375D, 0.3125D, 0.0625D, 0.625D, 0.6875D);
-
+    
 	public ColoredLightButton(String name) {
 		super(name);
 	}
 
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		//needed logic from both levers AND buttons here due to the lever inheritance doing things differently from buttons
+	    {
+	        boolean flag = ((Boolean)state.getValue(POWERED)).booleanValue();
+	        switch ((BlockLever.EnumOrientation)state.getValue(FACING))
+	        {
+	            case EAST:
+	            default:
+	                return flag ? AABB_EAST_ON : AABB_EAST_OFF;
+	            case WEST:
+	                return flag ? AABB_WEST_ON : AABB_WEST_OFF;
+	            case SOUTH:
+	                return flag ? AABB_SOUTH_ON : AABB_SOUTH_OFF;
+	            case NORTH:
+	                return flag ? AABB_NORTH_ON : AABB_NORTH_OFF;
+	            case UP_Z:
+	            case UP_X:
+	                return flag ? AABB_UP_ON : AABB_UP_OFF;
+	            case DOWN_X:
+	            case DOWN_Z:
+	                return flag ? AABB_DOWN_ON : AABB_DOWN_OFF;
+	        }
+	    }
+	}
 }
